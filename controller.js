180940a8d34a242b5360dec8913dec51e478f247
Abdul Aziz <cvabdulcom@gmail.com>
2021-06-06 11:12:53 +0700

    Berhasil hapus data

--- controller.js
+++ controller.js
@@ -20,8 +20,8 @@ exports.datamahasiswa = function(req, res){
 
 // menampilkan data mahasiswa berdasarkan id
 exports.datamahasiswaid = function(req, res){
-    let id = req.params.id
-    connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa=?", [id],
+    let id_mahasiswa = req.params.id_mahasiswa
+    connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa=?", [id_mahasiswa],
         function(error, rows, fields){
             if(error){
                 connection.log(error)
@@ -44,6 +44,39 @@ exports.tambahmahasiswa = function(req, res){
                 console.log(error)
             }else{
                 response.ok('Berhasil tambah data', res)
+            }
+        }
+    )
+}
+
+// mengubah data mahasiswa
+exports.ubahmahasiswa = function(req, res){
+    var id_mahasiswa = req.body.id_mahasiswa
+    var nim = req.body.nim
+    var nama = req.body.nama
+    var jurusan = req.body.jurusan
+
+    connection.query("UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?", [nim,nama,jurusan,id_mahasiswa],
+        function(error, rows, fields){
+            if(error){
+                console.log(error)
+            }else{
+                response.ok('Berhasil ubah mahasiswa', res)
+            }
+        }
+    )
+}
+
+// menghapus data mahasiswa
+exports.hapusmahasiswa = function(req, res){
+    var id_mahasiswa = req.body.id_mahasiswa
+
+    connection.query("DELETE FROM mahasiswa WHERE id_mahasiswa=?",[id_mahasiswa],
+        function(error, rows, fields){
+            if(error){
+                console.log(error)
+            }else{
+                response.ok('Berhasil hapus mahasiswa', res)
             }
         }
     )
--- routes.js
+++ routes.js
@@ -9,9 +9,15 @@ module.exports = function(app){
     app.route('/datamahasiswa')
         .get(jsonku.datamahasiswa)
 
-    app.route('/datamahasiswa/:id')
+    app.route('/datamahasiswa/:id_mahasiswa')
         .get(jsonku.datamahasiswaid)
 
     app.route('/tambahmahasiswa')
         .post(jsonku.tambahmahasiswa)
+
+    app.route('/ubahmahasiswa')
+        .put(jsonku.ubahmahasiswa)
+
+    app.route('/hapusmahasiswa')
+        .delete(jsonku.hapusmahasiswa)
 }
