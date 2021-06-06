316624051ce02b23a872f21d6de3999617d50f7a
Abdul Aziz <cvabdulcom@gmail.com>
2021-06-06 10:25:46 +0700

    Berhasil menampilkan data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok('Aplikasi REST API Berjalan', res);
 }
+
+// manampilkan semua data mahasiswa
+exports.datamahasiswa = function(req, res){
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, field){
+        if(error){
+            connection.log(error)
+        }else{
+            response.ok(rows, res)
+        }
+    });
+}
